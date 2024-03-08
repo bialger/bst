53b14f7bd3d98e1fef15fa1abdaa9aff6024b668
bialger <artur.bigulov@yandex.ru>
2024-03-09 01:13:14 +0300

    Implemented InOrder.hpp

--- lib/tree/InOrder.hpp
+++ lib/tree/InOrder.hpp
@@ -13,8 +13,65 @@ class InOrder : public ITraversal {
   InOrder() = delete;
 
   explicit InOrder(const ITree<T>& tree) : tree_(tree) {};
- private:
+
+  [[nodiscard]] const ITreeNode* GetFirst() const override {
+    return GetMin(tree_.GetRoot());
+  }
+
+  [[nodiscard]] const ITreeNode* GetLast() const override {
+    return GetMax(tree_.GetRoot());
+  }
+
+  const ITreeNode* GetPredecessor(const ITreeNode* current) const override {
+    if (current->HasLeft()) {
+      return GetMax(current->GetLeft());
+    }
+
+    const ITreeNode* parent = current->GetParent();
+    const ITreeNode* predecessor = current;
+
+    while (parent != nullptr && predecessor == parent->GetLeft()) {
+      predecessor = parent;
+      parent = parent->GetParent();
+    }
+
+    return parent;
+  }
+
+  const ITreeNode* GetSuccessor(const ITreeNode* current) const override {
+    if (current->HasRight()) {
+      return GetMin(current->GetRight());
+    }
+
+    const ITreeNode* parent = current->GetParent();
+    const ITreeNode* successor = current;
+
+    while (parent != nullptr && successor == parent->GetRight()) {
+      successor = parent;
+      parent = parent->GetParent();
+    }
+
+    return parent;
+  }
+
+ protected:
   const ITree<T>& tree_;
+
+  [[nodiscard]] const ITreeNode* GetMin(const ITreeNode* current) const {
+    while (current->HasLeft()) {
+      current = current->GetLeft();
+    }
+
+    return current;
+  }
+
+  [[nodiscard]] const ITreeNode* GetMax(const ITreeNode* current) const {
+    while (current->HasRight()) {
+      current = current->GetRight();
+    }
+
+    return current;
+  }
 };
 
 } // bialger
